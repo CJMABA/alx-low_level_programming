dc116a439ece010fe29d3d67c409aaf9fbfa38f9
CJMABA <zanacelestine@gmail.com>
2023-08-30 09:25:23 -0700

    print rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _rint_rev_recursion - Prints a string in reverse
+ * _print_rev_recursion - Prints a string in reverse
  * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
