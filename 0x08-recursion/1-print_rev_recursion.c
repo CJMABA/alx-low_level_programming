bba681128150187cfc65e25e5cd3944c1cd37c0e
CJMABA <zanacelestine@gmail.com>
2023-08-30 09:10:49 -0700

    wild

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _rint_rev_recursion - Prints a string in reverse
+ * @s: the string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
